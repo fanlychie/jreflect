9dce486565110907e60d2815d047bdc07dcc39a5
fanlychie <fanlychie@yeah.net>
2017-03-03 17:24:08 +0800

    字段操作异常

---
+++ src/main/java/org/fanlychie/reflection/exception/FieldOperationException.java
@@ -0,0 +1,13 @@
+package org.fanlychie.reflection.exception;
+
+/**
+ * 字段操作异常
+ * Created by fanlychie on 2017/3/3.
+ */
+public class FieldOperationException extends RuntimeException {
+
+    public FieldOperationException(String message) {
+        super(message);
+    }
+
+}
