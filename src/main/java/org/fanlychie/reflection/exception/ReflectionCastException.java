c926ac9a249e9190b91e69713a94b94501df34fa
fanlychie <fanlychie@yeah.net>
2017-03-03 17:24:21 +0800

    异常类

---
+++ src/main/java/org/fanlychie/reflection/exception/ReflectionCastException.java
@@ -0,0 +1,20 @@
+package org.fanlychie.reflection.exception;
+
+/**
+ * 运行时异常, 它内部包装了真实的非运行时异常对象, 通过 getCause 来取出真实异常
+ * Created by fanlychie on 2017/3/3.
+ */
+public class ReflectionCastException extends RuntimeException {
+
+    private Throwable cause;
+
+    public ReflectionCastException(Throwable throwable) {
+        this.cause = throwable;
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return cause;
+    }
+
+}
