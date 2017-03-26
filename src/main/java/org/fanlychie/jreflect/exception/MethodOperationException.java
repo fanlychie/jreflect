80690992bd55d6759c48a9824f8e5d0368cc4e31
fanlychie <fanlychie@yeah.net>
2017-03-27 00:46:15 +0800

    方法操作异常

---
+++ src/main/java/org/fanlychie/jreflect/exception/MethodOperationException.java
@@ -0,0 +1,13 @@
+package org.fanlychie.jreflect.exception;
+
+/**
+ * 方法操作异常
+ * Created by fanlychie on 2017/3/20.
+ */
+public class MethodOperationException extends RuntimeException {
+
+    public MethodOperationException(String message) {
+        super(message);
+    }
+
+}
